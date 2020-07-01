2eb30877d9e2210ad5dc786a89781b1747227dea
Alexis J. Villegas <alexis@ajvillegas.com>
2020-06-30 17:00:10 -0700

    Update syntax

--- admin/partials/ajv-portfolio-metabox-markup.php
+++ admin/partials/ajv-portfolio-metabox-markup.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Provide an admin area view for the meta boxes.
  *
@@ -13,75 +12,74 @@
  */
 
 ?>
-
 <table class="form-table">
 	<tbody>
 		<tr valign="top">
 			<th scope="row">
-				<label for="_ajv_portfolio_description"><?php _e( 'Short Description', 'ajv-portfolio' ); ?></label>
+				<label for="_ajv_portfolio_description"><?php echo esc_html__( 'Short Description', 'ajv-portfolio' ); ?></label>
 			</th>
 			<td>
 				<p>
-					<textarea class="widefat" rows="4" cols="78" name="_ajv_portfolio_description" id="_ajv_portfolio_description"><?php if ( isset ( $portfolio_stored_meta['_ajv_portfolio_description'] ) ) echo esc_attr( $portfolio_stored_meta['_ajv_portfolio_description'][0] ); ?></textarea>
+					<textarea class="widefat" rows="4" cols="78" name="_ajv_portfolio_description" id="_ajv_portfolio_description"><?php echo isset( $portfolio_stored_meta['_ajv_portfolio_description'] ) ? esc_attr( $portfolio_stored_meta['_ajv_portfolio_description'][0] ) : ''; ?></textarea>
 				</p>
 				<p>
-					<span class="description"><em><?php _e( 'Enter a short description for the project.', 'ajv-portfolio' ) ?></em></span>
+					<span class="description"><em><?php echo esc_html__( 'Enter a short description for the project.', 'ajv-portfolio' ); ?></em></span>
 				</p>
 			</td>
 		</tr>
-		
+
 		<tr valign="top">
 			<th scope="row">
-				<label for="_ajv_portfolio_client"><?php _e( 'Client Name', 'ajv-portfolio' ); ?></label>
+				<label for="_ajv_portfolio_client"><?php echo esc_html__( 'Client Name', 'ajv-portfolio' ); ?></label>
 			</th>
 			<td>
 				<p>
-					<input class="widefat" type="text" name="_ajv_portfolio_client" id="_ajv_portfolio_client" value="<?php if ( isset ( $portfolio_stored_meta['_ajv_portfolio_client'] ) ) echo esc_attr( $portfolio_stored_meta['_ajv_portfolio_client'][0] ); ?>">
+					<input class="widefat" type="text" name="_ajv_portfolio_client" id="_ajv_portfolio_client" value="<?php echo isset( $portfolio_stored_meta['_ajv_portfolio_client'] ) ? esc_attr( $portfolio_stored_meta['_ajv_portfolio_client'][0] ) : ''; ?>">
 				</p>
 				<p>
-					<span class="description"><em><?php _e( 'Enter the client\'s name.', 'ajv-portfolio' ) ?></em></span>
+					<span class="description"><em><?php echo esc_html__( 'Enter the client\'s name.', 'ajv-portfolio' ); ?></em></span>
 				</p>
 			</td>
 		</tr>
-		
+
 		<tr valign="top">
 			<th scope="row">
-				<label for="_ajv_portfolio_company"><?php _e( 'Client Company', 'ajv-portfolio' ); ?></label>
+				<label for="_ajv_portfolio_company"><?php echo esc_html__( 'Client Company', 'ajv-portfolio' ); ?></label>
 			</th>
 			<td>
 				<p>
-					<input class="widefat" type="text" name="_ajv_portfolio_company" id="_ajv_portfolio_company" value="<?php if ( isset ( $portfolio_stored_meta['_ajv_portfolio_company'] ) ) echo esc_attr( $portfolio_stored_meta['_ajv_portfolio_company'][0] ); ?>">
+					<input class="widefat" type="text" name="_ajv_portfolio_company" id="_ajv_portfolio_company" value="<?php echo isset( $portfolio_stored_meta['_ajv_portfolio_company'] ) ? esc_attr( $portfolio_stored_meta['_ajv_portfolio_company'][0] ) : ''; ?>">
 				</p>
 				<p>
-					<span class="description"><em><?php _e( 'Enter the name of the client\'s company, business, or organization.', 'ajv-portfolio' ) ?></em></span>
+					<span class="description"><em><?php echo esc_html__( 'Enter the name of the client\'s company, business, or organization.', 'ajv-portfolio' ); ?></em></span>
 				</p>
 			</td>
 		</tr>
-		
+
 		<tr valign="top">
 			<th scope="row">
-				<label for="_ajv_portfolio_url"><?php _e( 'Link URL', 'ajv-portfolio' ); ?></label>
+				<label for="_ajv_portfolio_url"><?php echo esc_html__( 'Link URL', 'ajv-portfolio' ); ?></label>
 			</th>
 			<td>
 				<p>
-					<input class="widefat" type="text" name="_ajv_portfolio_url" id="_ajv_portfolio_url" value="<?php if ( isset ( $portfolio_stored_meta['_ajv_portfolio_url'] ) ) echo esc_url( $portfolio_stored_meta['_ajv_portfolio_url'][0] ); ?>" placeholder="http://...">
+					<input class="widefat" type="text" name="_ajv_portfolio_url" id="_ajv_portfolio_url" value="<?php echo isset( $portfolio_stored_meta['_ajv_portfolio_url'] ) ? esc_url( $portfolio_stored_meta['_ajv_portfolio_url'][0] ) : ''; ?>" placeholder="http://...">
 				</p>
 				<p>
-					<span class="description"><em><?php _e( 'Enter the link to the client\'s site, or the web page where the work is displayed.', 'ajv-portfolio' ) ?></em></span>
+					<span class="description"><em><?php echo esc_html__( 'Enter the link to the client\'s site, or the web page where the work is displayed.', 'ajv-portfolio' ); ?></em></span>
 				</p>
 			</td>
 		</tr>
-		
+
 		<tr valign="top">
 			<th scope="row">
-				<label for="_ajv_portfolio_url_text"><?php _e( 'Link Text', 'ajv-portfolio' ); ?></label>
+				<label for="_ajv_portfolio_url_text"><?php echo esc_html__( 'Link Text', 'ajv-portfolio' ); ?></label>
 			</th>
 			<td>
 				<p>
-					<input class="widefat" type="text" name="_ajv_portfolio_url_text" id="_ajv_portfolio_url_text" value="<?php if ( isset ( $portfolio_stored_meta['_ajv_portfolio_url_text'] ) ) echo esc_attr( $portfolio_stored_meta['_ajv_portfolio_url_text'][0] ); ?>">
+					<input class="widefat" type="text" name="_ajv_portfolio_url_text" id="_ajv_portfolio_url_text" value="<?php echo isset( $portfolio_stored_meta['_ajv_portfolio_url_text'] ) ? esc_attr( $portfolio_stored_meta['_ajv_portfolio_url_text'][0] ) : ''; ?>">
 				</p>
 				<p>
-					<span class="description"><em><?php _e( 'Enter the link\'s anchor text.', 'ajv-portfolio' ) ?></em></span>
+					<span class="description"><em><?php echo esc_html__( 'Enter the link\'s anchor text.', 'ajv-portfolio' ); ?></em></span>
 				</p>
 			</td>
 		</tr>
